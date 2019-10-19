6cc1b0c9038a25c77f5bae31ef3fbe966e88db49
Laurent BERGE <laurent.berge@uni.lu>
2019-10-19 11:25:39 +0200

    reorg.

--- R/misc.R
+++ R/misc.R
@@ -20,9 +20,7 @@
 #'
 setHdd_extract.cap = function(sizeMB = 1000){
 
-	if(length(sizeMB) != 1 || !is.numeric(sizeMB) || is.na(sizeMB) || sizeMB < 0){
-		stop("Argument sizeMB must be a positive scalar.")
-	}
+	check_arg(sizeMB, "singleNumericGT0")
 
 	options("hdd_extract.cap" = sizeMB)
 }
@@ -40,14 +38,157 @@ getHdd_extract.cap = function(){
 	x
 }
 
-setHdd_extract.cap()
+####
+#### HDD utilities ####
+####
+
+object_size = function(x){
+	if("hdd" %in% class(x)){
+		res = tail(x$.size_cum, 1)
+	} else {
+		res = utils::object.size(x)
+	}
+	res
+}
+
+numID = function(x){
+	if(ncol(x) == 1){
+		x = x[[1]]
+	} else {
+		# we recreate the ids using rowid
+		vars = names(x)
+		x[, xxOBSxx := 1:.N]
+		setorderv(x, vars)
+
+		x[, xxNEWIDxx := cumsum(sign(1 - c(-100, diff(rowidv(x, vars)))))]
+		setorder(x, xxOBSxx)
+		x = x[["xxNEWIDxx"]]
+	}
+	return(x)
+}
+
+
+find_split = function(x){
+	# This function to find where to cut a file.
+	# we do not want the same value to be in two different files
+
+	x = numID(x)
+
+	n = length(x)
+	obs_mid = round(n/2)
+	v_left = x[obs_mid]
+	v_right = x[obs_mid + 1]
+
+	if(v_left != v_right){
+		return(obs_mid)
+	} else {
+		# then we need to split differently
+		if(v_left != x[n]){
+			obs_mid = obs_mid + which.max(x[(obs_mid+1):n] != v_left) - 1
+		} else if(v_left != x[1]){
+			obs_mid = which.max(x[1:obs_mid] == v_left) - 1
+		} else {
+			stop("No middle value found. The two files could not be split, revise the code to allow merging the files automatically.")
+		}
+		return(obs_mid)
+	}
+}
+
+find_n_split = function(x, key, nfiles){
+	# This function to find where to cut a file.
+	# we do not want the same value to be in two different files
+	# nfiles: the new number of files
+	# returns a vector of the beginning observations
+
+	nfiles_origin = length(x$.nrow)
+	n_all = x$.row_cum[nfiles_origin]
+
+	# preliminary starting point of each file
+	start = floor(seq(1, n_all, by = n_all/nfiles))
+	start = c(start[1:nfiles], n_all)
+
+	DELTA = 10000 # value of the increment
+	if(n_all/nfiles + 2 < DELTA){
+		DELTA = floor(n_all/nfiles) - 1
+	}
+
+	# We find the right starting point of each file
+	for(i in 2:nfiles){
+		start_tmp = start[i]
+		id = numID(x[start_tmp + 0:1, j = key, with = FALSE])
+		id_left = id[1]
+
+		if(id[1] != id[2]){
+			# OK! => nothing to do
+			next
+		} else {
+			# we go to the right
+			k = 1
+			while(TRUE){
+				if(start_tmp + k*DELTA > start[i + 1]){
+					stop("The hdd file with key could not be split in ", nfiles, " documents (because of too many identical keys). Reduce the number of documents.")
+				}
+
+				id = numID(x[start_tmp + c(0, k*DELTA), key, with = FALSE])
+				if(id[1] != id[2]){
+					# we find the right point!
+					id = numID(x[start_tmp + c(0, ((DELTA*(k-1)+1):(DELTA*k))), key, with = FALSE])
+					start_new = start_tmp + DELTA*(k-1) + which.max(id[-1] != id[1])
+					start[i] = start_new
+					break
+				} else {
+					k = k + 1
+				}
+			}
+		}
+	}
+
+	start_final = start[1:nfiles]
+	return(start_final)
+}
+
+obs = function(x, file){
+	# Finds the observation numbers of a hdd document by file
+
+	if(!"hdd" %in% class(x)){
+		stop("x must be a hdd file.")
+	}
+
+	if(missing(file)){
+		stop("file must be provided.")
+	}
+
+	n = length(x$.nrow)
+	control_variable(file, "integerVectorGT0")
+	if(any(file > n)){
+		stop("file cannot exceed ", n, ".")
+	}
+
+
+	row_cum = x$.row_cum
+	end = row_cum
+	start = (1 + c(0, row_cum))
+
+	# creation of the vector
+	res = list()
+	index = 0
+	for(i in file){
+		index = index + 1
+		res[[index]] = start[i]:end[i]
+	}
+
+	unlist(res)
+}
+
+myView <- function(x, title){
+	get("View", envir = as.environment("package:utils"))(x, title)
+}
 
 
 
 ####
-#### Utilities ####
+#### Other Utilities ####
 ####
-
 
 addCommas = function(x){
 
@@ -199,13 +340,13 @@ osize = function(x){
 
 	if(n < 3){
 		# cat(size, " Octets.\n")
-		res = paste0(size, " Octets.")
+		res = paste0(size, " Bytes.")
 	} else if(n < 6){
 		# cat(mysignif(size/1000, 20, 2), " Ko.\n")
-		res = paste0(mysignif(size/1000, 3, 0), " Ko.")
+		res = paste0(mysignif(size/1000, 3, 0), " KB.")
 	} else {
 		# cat(addCommas(mysignif(size/1000000, 20, 2)), " Mo.\n")
-		res = paste0(addCommas(mysignif(size/1000000, 3, 0)), " Mo.")
+		res = paste0(addCommas(mysignif(size/1000000, 3, 0)), " MB.")
 	}
 
 	class(res) = "osize"
@@ -216,285 +357,6 @@ osize = function(x){
 ggrepl = function(pattern, x){
 	x[grepl(pattern, x, perl = TRUE)]
 }
-
-
-####
-#### CONTROL ####
-####
-
-
-control_variable = function(x, myType, prefix, name, charVec, mustBeThere = FALSE){
-	# Format of my types:
-	#   - single => must be of lenght one
-	#   - Vector => must be a vector
-	#   - Matrix => must be a matrix
-	#   - GE/GT/LE/LT: greater/lower than a given value
-	#   - predefinedType => eg: numeric, integer, etc
-	#   - match.arg => very specific => should match the charVec
-	#   - noNA: NAs not allowed
-	#   - null: null type allowed
-	# If there is a parenthesis => the class must be of specified types:
-	# ex: "(list, data.frame)" must be a list of a data.frame
-
-	ignore.case = TRUE
-
-	if(missing(prefix)){
-		msg = deparse(sys.calls()[[sys.nframe()-1]])[1] # call can have svl lines
-		if(length(msg) > 1) browser()
-		nmax = 40
-		if(nchar(msg) > nmax) msg = paste0(substr(msg, 1, nmax-1), "...")
-		prefix = paste0(msg, ": ")
-	}
-
-	x_name = deparse(substitute(x))
-	if(missing(name)) name = x_name
-
-	firstMsg = paste0(prefix, "The argument '", name, "' ")
-
-	if(missing(x)){
-		if(mustBeThere){
-			stop(firstMsg, "is missing => it must be provided.", call. = FALSE)
-		} else {
-			return(invisible(NULL))
-		}
-	}
-
-
-	# simple function to extract a pattern
-	# ex: if my type is VectorIntegerGE1 => myExtract("GE[[:digit:]]+","VectorIntegerGE1") => 1
-	myExtract = function(expr, text, trim=2){
-		start = gregexpr(expr,text)[[1]] + trim
-		length = attr(start,"match.length") - trim
-		res = substr(text,start,start+length-1)
-		as.numeric(res)
-	}
-
-	#
-	# General types handling
-	#
-
-	loType = tolower(myType)
-
-	# null type is caught first
-	if(grepl("null", loType)){
-		if(is.null(x)) return(invisible(NULL))
-	}
-
-	if(grepl("single", loType)){
-		if(length(x) != 1) stop(firstMsg,"must be of length one.", call. = FALSE)
-	}
-
-	if(grepl("vector", loType)){
-		if(!checkVector(x)) stop(firstMsg,"must be a vector.", call. = FALSE)
-		if(is.list(x)) stop(firstMsg,"must be a vector (and not a list).", call. = FALSE)
-	}
-
-	res = checkTheTypes(loType, x)
-	if(!res$OK) stop(firstMsg,res$message, call. = FALSE)
-
-	# # INTEGER is a restrictive type that deserves some explanations (not included in getTheTypes)
-	# if(grepl("integer",loType)){
-	#     if(grepl("single",loType)){
-	#         if(!is.numeric(x)) stop(firstMsg,"must be an integer (right now it is not even numeric).", call. = FALSE)
-	#         if(!(is.integer(x) || x%%1==0)) stop(firstMsg,"must be an integer.", call. = FALSE)
-	#     } else {
-	#         if(!is.numeric(x)) stop(firstMsg,"must be composed of integers (right now it is not even numeric).", call. = FALSE)
-	#         if(!(is.integer(x) || all(x%%1==0))) stop(firstMsg,"must be composed of integers.", call. = FALSE)
-	#     }
-	# }
-
-	if(grepl("nona", loType)){
-		if(any(is.na(x))){
-			stop(firstMsg,"contains NAs, this is not allowed.", call. = FALSE)
-		}
-	}
-
-	# GE: greater or equal // GT: greater than // LE: lower or equal // LT: lower than
-	if(is.numeric(x)){
-		x = x[!is.na(x)]
-
-		if(grepl("ge[[:digit:]]+",loType)){
-			n = myExtract("ge[[:digit:]]+", loType)
-			if( !all(x>=n) ) stop(firstMsg,"must be greater than, or equal to, ", n, ".", call. = FALSE)
-		}
-		if(grepl("gt[[:digit:]]+",loType)){
-			n = myExtract("gt[[:digit:]]+", loType)
-			if( !all(x>n) ) stop(firstMsg,"must be strictly greater than ", n, ".", call. = FALSE)
-		}
-		if(grepl("le[[:digit:]]+",loType)){
-			n = myExtract("le[[:digit:]]+", loType)
-			if( !all(x<=n) ) stop(firstMsg,"must be lower than, or equal to, ", n, ".", call. = FALSE)
-		}
-		if(grepl("lt[[:digit:]]+",loType)){
-			n = myExtract("lt[[:digit:]]+", loType)
-			if( !all(x<n) ) stop(firstMsg,"must be strictly lower than ", n, ".", call. = FALSE)
-		}
-	}
-
-	#
-	# Specific Types Handling
-	#
-
-	if(grepl("match.arg", loType)){
-		if(ignore.case){
-			x = toupper(x)
-			newCharVec = toupper(charVec)
-		} else {
-			newCharVec = charVec
-		}
-
-		if( is.na(pmatch(x, newCharVec)) ){
-			n = length(charVec)
-			if(n == 1){
-				msg = paste0("'",charVec,"'")
-			} else {
-				msg = paste0("'", paste0(charVec[1:(n-1)], collapse="', '"), "' or '",charVec[n],"'")
-			}
-			stop(firstMsg,"must be one of:\n",msg,".", call. = FALSE)
-		} else {
-			qui = pmatch(x, newCharVec)
-			return(charVec[qui])
-		}
-	}
-}
-
-matchTypeAndSetDefault = function(myList, myDefault, myTypes, prefix){
-	# Cette fonction:
-	#   i) verifie que tous les elements de la liste sont valides
-	#   ii) mes les valeurs par defauts si elles certaines valeurs sont manquantes
-	#   iii) Envoie des messages d'erreur si les typages ne sont pas bons
-	# En fait cette fonction "coerce" myList en ce qu'il faudrait etre (donne par myDefault)
-
-	# 1) check that the names of the list are valid
-	if(is.null(myList)) myList = list()
-	list_names = names(myList)
-
-	if(length(list_names)!=length(myList) || any(list_names=="")){
-		stop(prefix,"The elements of the list should be named.", call. = FALSE)
-	}
-
-	obj_names = names(myDefault)
-
-	isHere = pmatch(list_names,obj_names)
-
-	if(anyNA(isHere)){
-		if(sum(is.na(isHere))==1) stop(prefix, "The following argument is not defined: ",paste(list_names[is.na(isHere)],sep=", "), call. = FALSE)
-		else stop(prefix, "The following arguments are not defined: ",paste(list_names[is.na(isHere)],sep=", "), call. = FALSE)
-	}
-
-	# 2) We set the default values and run Warnings
-	res = list()
-	for(i in 1:length(obj_names)){
-		obj = obj_names[i]
-		qui = which(isHere==i) # qui vaut le numero de l'objet dans myList
-		type = myTypes[i] # we extract the type => to control for "match.arg" type
-		if(length(qui)==0){
-			# we set to the default if it's missing
-			if(type == "match.arg") {
-				res[[obj]] = myDefault[[i]][1]
-			} else {
-				res[[obj]] = myDefault[[i]]
-			}
-		} else {
-			# we check the integrity of the value
-			val = myList[[qui]]
-			if(type == "match.arg"){
-				# If the value is to be a match.arg => we use our controls and not
-				# directly the one of the function match.arg()
-				charVec = myDefault[[i]]
-				control_variable(val, "singleCharacterMatch.arg", prefix, obj, charVec)
-				val = match.arg(val, charVec)
-			} else {
-				control_variable(val, type, prefix, obj)
-			}
-
-			res[[obj]] = val
-		}
-	}
-
-	return(res)
-}
-
-
-
-checkTheTypes = function(str, x){
-	# This function takes in a character string describing the types of the
-	# element x => it can be of several types
-
-	# types that are controlled for:
-	allTypes = c("numeric", "integer", "character", "logical", "list", "data.frame", "matrix", "factor", "formula")
-
-	OK = FALSE
-	message = c()
-
-	for(type in allTypes){
-
-		if(grepl(type, str)){
-
-			# we add the type of the control
-			message = c(message, type)
-
-			if(type == "numeric"){
-				if(!OK & is.numeric(x)){
-					OK = TRUE
-				}
-			} else if(type == "integer"){
-				if(is.numeric(x) && (is.integer(x) || all(x%%1==0))){
-					OK = TRUE
-				}
-			} else if(type == "character"){
-				if(is.character(x)){
-					OK = TRUE
-				}
-			} else if(type == "logical"){
-				if(is.logical(x)){
-					OK = TRUE
-				}
-			} else if(type == "list"){
-				if(is.list(x)){
-					OK = TRUE
-				}
-			} else if(type == "data.frame"){
-				if(is.data.frame(x)){
-					OK = TRUE
-				}
-			} else if(type == "matrix"){
-				if(is.matrix(x)){
-					OK = TRUE
-				}
-			} else if(type == "factor"){
-				if(is.factor(x)){
-					OK = TRUE
-				}
-			}  else if(type == "formula"){
-				if(length(class(x)) == 1 && class(x) == "formula"){
-					OK = TRUE
-				}
-			}
-		}
-
-		if(OK) break
-	}
-
-	if(length(message) == 0){
-		OK = TRUE #ie there is no type to be searched
-	} else if(length(message) >= 3){
-		n = length(message)
-		message = paste0("must be of type: ",  paste0(message[1:(n-1)], collapse = ", "), " or ", message[n], ".")
-	} else {
-		message = paste0("must be of type: ",  paste0(message, collapse = " or "), ".")
-	}
-
-
-	return(list(OK=OK, message=message))
-}
-
-
-
-
-
-
-
 
 
 
